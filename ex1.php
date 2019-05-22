5b70093638b8a8c88be0dee116c80fe52de1905f
Sophal24 <sophalisco23@gmail.com>
2019-05-23 01:23:41 +0700

    Exercise1

---
+++ ex1.php
@@ -0,0 +1,38 @@
+<?php
+	
+	$arr = range('a', 'z');
+	print_r($arr);
+
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+
+	// Start 
+
+	$text = "hello world, i am cs";
+	echo strlen($text);
+	$shift = 3;
+
+	for ($i=0; $i < strlen($text) ; $i++) { 
+		echo " $text[$i] ";
+	}
+
+	echo "<br>";
+
+	for ($i=0; $i < strlen($text) ; $i++) { 
+		for ($j=0; $j < sizeof($arr); $j++) { 
+			# code...
+			// echo "$text[$i]";
+
+			if ("$text[$i]" == "$arr[$j]") {
+				# code...
+				$a = $j + $shift ;
+				echo "$arr[$a]";
+						
+			}			
+		}echo "<br>";
+
+	}
+
+?>
